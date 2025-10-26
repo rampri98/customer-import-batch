1132258cdf328466d49bf9e391a57cc9b29cac19
rampri98 <ramyapriyadarshini78@gmail.com>
2025-10-26 22:00:17 +0530

    Initial commit

---
+++ src/main/java/com/example/customer_import_batch/CustomerImportBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_import_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerImportBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerImportBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customer_import_batch/CustomerImportBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer_import_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerImportBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
